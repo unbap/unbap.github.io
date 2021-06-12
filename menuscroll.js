24b01f40c3403385c4492cac0dc734558accd6be
redstonecold <hsc010616@gmail.com>
2021-06-13 04:31:44 +0900

    메뉴바 트랜지션 없이 구현

---
+++ menuscroll.js
@@ -0,0 +1,17 @@
+function scrollToPart1() {
+    var menuHeight = document.querySelector(".menuBar").offsetHeight;
+    var location = document.querySelector(".menuBar").offsetTop;
+    window.scrollTo({ top: location - menuHeight, behavior: 'smooth' });
+}
+
+function scrollToPart2() {
+    var menuHeight = document.querySelector(".menuBar").offsetHeight;
+    var location = document.querySelector(".part-2 .part-title").offsetTop;
+    window.scrollTo({ top: location - menuHeight + 100, behavior: 'smooth' });
+}
+
+function scrollToPart3() {
+    var menuHeight = document.querySelector(".menuBar").offsetHeight;
+    var location = document.querySelector(".part-3 .part-title").offsetTop;
+    window.scrollTo({ top: location - menuHeight + 120, behavior: 'smooth' });
+}
